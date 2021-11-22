abfe0370bc5aabe6691251c3c0ebec4cc525a3e3
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-11-22 17:53:39 +0300

    Format price in US dollars

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    // NOTE: we divide number by hundred to get a price in dollars instead of cents
+  }).format(number / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
