2b14e0545ab81ecaa9097427c6724fdefb2e21da
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-11-23 17:46:51 +0300

    Setup loading animation and error message

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
-import { useProductsContext } from '../context/products_context'
-import { single_product_url as url } from '../utils/constants'
-import { formatPrice } from '../utils/helpers'
+import React, { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useProductsContext } from '../context/products_context';
+import { single_product_url as url } from '../utils/constants';
+import { formatPrice } from '../utils/helpers';
 import {
   Loading,
   Error,
@@ -10,13 +10,46 @@ import {
   AddToCart,
   Stars,
   PageHero,
-} from '../components'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+} from '../components';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
-}
+  // NOTE: useParams() hook helps to access the URL parameters from a current route
+  // console.log(useParams()); // log single product id
+  const { id } = useParams();
+  // NOTE: useHistory() hook helps to access the history object, which is used to navigate programmatically to other routes using push and replace methods
+  const history = useHistory();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+
+  useEffect(() => {
+    console.log(error);
+    if (error) {
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
+    }
+  }, [error]);
+
+  // console.log(product); // log single product object with the product data
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+
+  return <h4>single product page</h4>;
+};
 
 const Wrapper = styled.main`
   .product-center {
@@ -50,6 +83,6 @@ const Wrapper = styled.main`
       font-size: 1.25rem;
     }
   }
-`
+`;
 
-export default SingleProductPage
+export default SingleProductPage;
