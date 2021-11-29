32256ebd3b31b4e0b882e43faa244f91cf98969e
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-11-29 17:23:30 +0300

    Star rating manual approach

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -53,6 +53,7 @@ const SingleProductPage = () => {
     price,
     description,
     stock,
+    stars,
     reviews,
     id: sku,
     company,
@@ -70,7 +71,7 @@ const SingleProductPage = () => {
           <ProductImages images={images} />
           <section className='content'>
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5 className='price'>{formatPrice(price)}</h5>
             <p className='desc'>{description}</p>
             <p className='info'>
